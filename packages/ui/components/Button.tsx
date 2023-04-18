4caac6b443380953978444b8815a685ee3b701d4
adultlee <seoungin1228@gmail.com>
2023-04-18 20:06:47 +0900

    feat: Button 컴포넌트 개발

--- packages/ui/components/Button.tsx
+++ packages/ui/components/Button.tsx
@@ -1,13 +1,43 @@
+import React from "react";
+import classnames from "classnames";
 import { css } from "@emotion/react";
+import "../scss/button.scss";
 
-export const Button = () => {
-	return (
-		<button
-			css={css`
-				background-color: black;
-			`}
-		>
-			Boop
-		</button>
-	);
+type ButtonProps = {
+	variant?: "primary" | "secondary" | "danger"; // variant prop 타입 정의
+	disabled?: boolean;
+	className?: string;
+	children: React.ReactNode;
+	onClick?: React.MouseEventHandler<HTMLElement>;
 };
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+	(props, ref) => {
+		const {
+			variant = "primary",
+			disabled = false,
+			className,
+			children,
+			onClick,
+			...rest
+		} = props;
+
+		return (
+			<button
+				ref={ref}
+				{...rest}
+				onClick={onClick}
+				disabled={disabled}
+				className={classnames("button", `button--${variant}`, className)}
+				css={css`
+					&:disabled {
+						opacity: 0.5;
+						cursor: not-allowed;
+					}
+				`}
+			>
+				{children}
+			</button>
+		);
+	}
+);
