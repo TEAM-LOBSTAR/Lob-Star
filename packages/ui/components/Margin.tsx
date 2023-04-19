c59ab58ea0c5afd2f6db6450c307e9c8c701482d
adultlee <seoungin1228@gmail.com>
2023-04-19 16:18:36 +0900

    fix: 컨벤션 수정
    외부에 style 로 나가있던, css 속성을 모두  inline으로 수정합니다. interface 대신 type으로 통일합니다

--- packages/ui/components/Margin.tsx
+++ packages/ui/components/Margin.tsx
@@ -1,16 +1,18 @@
 import { css } from "@emotion/react";
 
-interface MarginProps {
+type MarginProps = {
 	dir: "row" | "col";
 	size: number;
-}
+};
 
 export const Margin = ({ dir, size }: MarginProps) => {
-	const style = css`
-		display: block;
-		width: ${dir === "row" ? `${size}px` : "100%"};
-		height: ${dir === "col" ? `${size}px` : "100%"};
-	`;
-
-	return <div css={style} />;
+	return (
+		<div
+			css={css`
+				display: block;
+				width: ${dir === "row" ? `${size}px` : "100%"};
+				height: ${dir === "col" ? `${size}px` : "100%"};
+			`}
+		/>
+	);
 };
